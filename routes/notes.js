a1f31c69d2d04722d5b8038b9b149a7adf3731c0
AngadBatth <andyybatth@gmail.com>
2023-05-22 22:52:55 -0400

    changes to notes.js + added comments

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,40 @@
+// Including express + Router function
 const notes = require("express").Router();
+
+// Including FS + Crypto packages for UUID
 const { writeFile } = require("fs");
 const { randomUUID } = require("crypto");
 
+// Setting data variable to db.json path
 let data = require("../db/db.json");
 
+// UUID generation
 const addUUID = (obj) => {
   obj.id = randomUUID();
   return obj;
 };
 
-const updateDB = (data) => {
+// Function to write data into database
+const updateDatabase = (data) => {
   writeFile("./db/db.json", JSON.stringify(data), (err) => {
     if (err) console.error(err);
   });
 };
 
+// Notes API below
 notes.get("/", (req, res) => {
   res.json(data);
 });
 
 notes.post("/", (req, res) => {
   data.push(addUUID(req.body));
-  updateDB(data);
+  updateDatabase(data);
   res.status(201).json(data);
 });
 
 notes.delete("/:id", (req, res) => {
   data = data.filter((note) => note.id !== req.params.id);
-  updateDB(data);
+  updateDatabase(data);
   res.json(data);
 });
 
